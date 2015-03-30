13c93306472d52413ace0cd1d768a2169863d00a
Jordi Pradel <jordi.pradel@agilogy.com>
2015-03-30 16:07:45 +0200

    Test open bounds and closed bounds intervals

--- src/test/scala/com/agilogy/interval/IntervalTest.scala
+++ src/test/scala/com/agilogy/interval/IntervalTest.scala
@@ -13,4 +13,23 @@ class IntervalTest extends FlatSpec {
     assert(i.includes(0) === true)
   }
 
+  behavior of "Open ended interval"
+
+  it should "include any value in the interval, but not the bounds" in {
+    val i = Interval[Float](Some(OpenBound(2.3f)), Some(OpenBound(2.4f)))
+    assert(i.includes(2.30001f) === true)
+    assert(i.includes(2.39999f) === true)
+    assert(i.includes(2.3f) === false)
+    assert(i.includes(2.4f) === false)
+  }
+
+  behavior of "Close ended interval"
+
+  it should "include any value in the interval and its bounds" in {
+    val i = Interval[Float](Some(ClosedBound(2.3f)), Some(ClosedBound(2.4f)))
+    assert(i.includes(2.30001f) === true)
+    assert(i.includes(2.39999f) === true)
+    assert(i.includes(2.3f) === true)
+    assert(i.includes(2.4f) === true)
+  }
 }
